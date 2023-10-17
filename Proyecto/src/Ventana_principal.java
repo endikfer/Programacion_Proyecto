0f240a16ac6bc52e964d898cc6610501eeb763f9
Endika <Endika@10.164.50.226>
2023-10-17 08:41:54 +0200

    Ventana creada con titulo tamaño y ejecucion de ella, sin ningun elemento en ella

--- Proyecto/src/Ventana_principal.java
+++ Proyecto/src/Ventana_principal.java
@@ -1,4 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Ventana_principal {
+public class Ventana_principal extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Ventana_principal(){
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(640, 480);
+        setTitle("Ventana Principal");
+        setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new Ventana_principal();
+            }
+
+        });
+    }
 
 }
