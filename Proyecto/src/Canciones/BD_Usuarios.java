03a5ee61c9dcf4b5f43376511a6b1199e3d00e7c
Endika <fernandez.endika@opendeusto.es>
2023-10-30 11:24:23 +0100

    Pruebas en el programa

--- Proyecto/src/Canciones/BD_Usuarios.java
+++ Proyecto/src/Canciones/BD_Usuarios.java
@@ -1,15 +1,12 @@
 package Canciones;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import Ventanas.Ventana_usuario;
 
 
 
