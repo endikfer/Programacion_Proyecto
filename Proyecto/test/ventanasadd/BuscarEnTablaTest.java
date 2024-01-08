4a7277cda368f0f88ec7947c7aa8a39a202e8b1b
Endika <fernandez.endika@opendeusto.es>
2024-01-08 12:43:23 +0100

    estructura para JUnite

---
+++ Proyecto/test/ventanasadd/BuscarEnTablaTest.java
@@ -0,0 +1,16 @@
+package ventanasadd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuscarEnTablaTest {
+	
+//	@Before
+
+	@Test
+	public void testBuscar() {
+		assertEquals(0, 0);
+	}
+
+}
