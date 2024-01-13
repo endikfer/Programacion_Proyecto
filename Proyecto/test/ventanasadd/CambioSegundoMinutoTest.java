364d8445cdc71c45f8e012d9e2b294c88111cad0
Endika <fernandez.endika@opendeusto.es>
2024-01-13 14:48:37 +0100

    Test de Cambiar de segundos a minutos hecho

---
+++ Proyecto/test/ventanasadd/CambioSegundoMinutoTest.java
@@ -0,0 +1,14 @@
+package ventanasadd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CambioSegundoMinutoTest {
+
+	@Test
+	public void cambioSectest() {
+		assertEquals("01:00", CambioSegundoMinuto.cambioSec(60));
+	}
+
+}
