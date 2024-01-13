efdd19178a3cebb3335f79a69b68f239ab769825
Endika <fernandez.endika@opendeusto.es>
2024-01-13 16:15:38 +0100

    Test para Comparar la duracion hecho, y terminado todos los tests

---
+++ Proyecto/test/ventanasadd/ComparadorDuraTest.java
@@ -0,0 +1,27 @@
+package ventanasadd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import canciones.Cancion;
+
+public class ComparadorDuraTest {
+	
+	private ComparadorDura com = new ComparadorDura();
+	private Cancion can1;
+	private Cancion can2;
+	
+	@Before
+	public void setup() {
+		can1 = new Cancion("a",20,"c","cd");
+		can2 = new Cancion("a",20,"c","cd");
+	}
+
+	@Test
+	public void test() {
+		assertEquals(0, com.compare(can1, can2));
+	}
+
+}
